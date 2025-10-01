ed8e734aa7145c59942dcf63a9af2122dd856d4d
mohamedhakkeem6472 <42826967+mohamedhakkeem6472@users.noreply.github.com>
2025-10-01 11:52:58 +0400

    Create User.java

---
+++ src/main/java/com/example/splitwise/model/User.java
@@ -0,0 +1,33 @@
+package com.example.splitwise.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String oauthId;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
