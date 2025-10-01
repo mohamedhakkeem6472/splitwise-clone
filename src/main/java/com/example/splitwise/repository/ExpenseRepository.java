afc7b552ebc03c22f9767744ee3a887a07bb1c82
mohamedhakkeem6472 <42826967+mohamedhakkeem6472@users.noreply.github.com>
2025-10-01 14:22:04 +0400

    Update and rename ExpenseRepository .java to ExpenseRepository.java

--- src/main/java/com/example/splitwise/repository/ExpenseRepository.java
+++ src/main/java/com/example/splitwise/repository/ExpenseRepository.java
@@ -1,8 +1,9 @@
 package com.example.splitwise.repository;
 
-import com.example.splitwise.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.splitwise.model.Expense;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
