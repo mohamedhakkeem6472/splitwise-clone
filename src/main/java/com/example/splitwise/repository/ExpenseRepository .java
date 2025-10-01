9b687853a5763b50744c422e15ce0b5055a1ca8d
mohamedhakkeem6472 <42826967+mohamedhakkeem6472@users.noreply.github.com>
2025-10-01 11:56:39 +0400

    Create ExpenseRepository .java

---
+++ src/main/java/com/example/splitwise/repository/ExpenseRepository .java
@@ -0,0 +1,9 @@
+package com.example.splitwise.repository;
+
+import com.example.splitwise.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+}
