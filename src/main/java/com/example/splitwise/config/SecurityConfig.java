1a9a9701c6d89390f44bec879d2d7f029c6daf6b
mohamedhakkeem6472 <42826967+mohamedhakkeem6472@users.noreply.github.com>
2025-10-01 16:43:08 +0400

    Update SecurityConfig.java

--- src/main/java/com/example/splitwise/config/SecurityConfig.java
+++ src/main/java/com/example/splitwise/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,9 +29,30 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+    
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    //Dummy data
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration registration = ClientRegistration.withRegistrationId("google")
+                .clientId("dummy-client-id")
+                .clientSecret("dummy-client-secret")
+                .authorizationGrantType(org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE) // 👈 FIXED
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+                .tokenUri("https://www.googleapis.com/oauth2/v4/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName("sub")
+                .clientName("Google")
+                .scope("openid", "profile", "email")
+                .build();
+
+        return new InMemoryClientRegistrationRepository(registration);
     }
 }
