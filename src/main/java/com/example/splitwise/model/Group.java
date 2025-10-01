d06c4d861ea45cb4868ab1ec273d8245c276c045
mohamedhakkeem6472 <42826967+mohamedhakkeem6472@users.noreply.github.com>
2025-10-01 11:53:42 +0400

    Create Group.java

---
+++ src/main/java/com/example/splitwise/model/Group.java
@@ -0,0 +1,32 @@
+package com.example.splitwise.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "groups")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "group_users",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    @Builder.Default
+    private Set<User> members = new HashSet<>();
+}
