9418a2b3c3fa05554912cb41158924c0fe85c138
mohamedhakkeem6472 <42826967+mohamedhakkeem6472@users.noreply.github.com>
2025-10-01 11:57:01 +0400

    Create SettlementRepository.java

---
+++ src/main/java/com/example/splitwise/repository/SettlementRepository.java
@@ -0,0 +1,9 @@
+package com.example.splitwise.repository;
+
+import com.example.splitwise.model.Settlement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SettlementRepository extends JpaRepository<Settlement, Long> {
+}
